bde1e56deae9a872152dd8c7807e3cfccf6f7693
Marc Pasquet <mpa78700@gmail.com>
2018-10-07 01:19:43 +0200

    add Functions.R

--- Functions.R
+++ Functions.R
@@ -153,3 +153,105 @@ print(grouped_violin_plot(iris, "Sepal.Length", "Species"))
 
 ## 5.a ----
 
+# Difference in the medians between two groups.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d containing the dependent variable, provided as a string
+# grouping_var: the name of a column of d containing a grouping variable, provided as a string
+# group1: the value of grouping_var that corresponds to the first group
+# group2: the value of grouping_var that corresponds to the second group
+#
+# RETURN VALUE:
+# The median value of var for the first group, minus the median value of var for the second
+# group.
+#
+difference_in_medians <- function(d, var, grouping_var, group1, group2) {
+  d_1 <- dplyr::filter(d, get(grouping_var) == group1)
+  d_2 <- dplyr::filter(d, get(grouping_var) == group2)
+  
+  result <- median(d_1[[var]]) - median(d_2[[var]])
+  
+  return(result)
+}
+
+difference_in_medians(iris, "Sepal.Width", "Species", "versicolor", "virginica")
+difference_in_medians(iris, "Sepal.Width", "Species", "virginica", "virginica")
+
+## 5.b ----
+
+# Randomize the order of a column.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d containing the variable to randomize,
+# provided as a string
+#
+# RETURN VALUE:
+# A data frame or tibble exactly the same as d, except with the order of
+# var permuted randomly.
+#
+randomize <- function(d, var) {
+  n <- nrow(d)
+  d[[var]] <- sample(d[[var]], n)
+    return(d)
+}
+
+iris$Sepal.Width[1:10]
+if(!exists(".Random.seed")) set.seed(NULL)
+previous_seed <- .Random.seed
+set.seed(1)
+randomize(iris, "Sepal.Width")$Sepal.Width[1:10]
+randomize(iris, "Species")$Species[1:10]
+randomize(iris, "Species")$Sepal.Width[1:10]
+set.seed(previous_seed)
+
+## 5.c ----
+
+# Perform a permutation test for two groups.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of the column in d on which the test statistic will be calculated,
+# provided as a string
+# grouping_var: the name of the column in d which gives the grouping
+# group1: the value of grouping_var corresponding to the first group
+# group2: the value of grouping_var corresponding to the second group
+# statistic: a function yielding a test statistic, which takes as input
+# a data frame, the name of a variable on which to calculate the
+# test statistic, the name of a grouping variable, the value of
+# the grouping variable corresponding to the first group, and
+# the value of the grouping variable corresponding to the second
+# group
+# n_samples: the number of permutation samples to draw (default: 9999)
+#
+
+# RETURN VALUE:
+#
+# A list containing two elements:
+#
+# - observed: the value of statistic() in d
+# - permuted: a vector containing the values of statistic() under n_samples
+# permutations
+#
+permutation_twogroups <- function(d, var, grouping_var, group1, group2, statistic,
+                                  n_samples=9999) {
+  observed_statistic <- statistic(d, var, grouping_var, group1, group2)
+  permutation_statistics <- rep(0, n_samples)
+  for (i in 1:n_samples) {
+    # YOUR CODE HERE: use randomize(...) to create a permutation and then
+    # fill in the vector permutation_statistics with the
+    # value of statistic(...) for this new permutation
+  }
+  result <- list(observed=observed_statistic,
+                 permuted=permutation_statistics)
+  return(result)
+}
+
+## 5.d ----
+
+## 5.e ----
+
+
+
+
