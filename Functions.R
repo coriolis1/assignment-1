618a5d6becc12737e3df3a6168c9a52dc6ebc278
Marc Pasquet <mpa78700@gmail.com>
2018-10-06 23:39:57 +0200

    add Functions.R

---
+++ Functions.R
@@ -0,0 +1,123 @@
+#### Exercice 3 ----
+
+## 3.a ----
+
+# Sum values in a column of a data frame.
+#
+# ARGUMENTS:
+# d: a data frame or tibble
+# var: the name of a column of d, provided as a string
+#
+# RETURN VALUE:
+# if the specified column exists and contains numbers, returns the sum of
+# all values in the column; otherwise, returns NULL
+
+sum_column <- function(d, var) {
+  result <- NULL
+  x <- d[[var]]
+  if (!is.null(x)) { 
+    
+    if (is.numeric(x)) 
+      result <- sum(x)
+  }
+  return(result)
+}
+
+print(sum_column(iris, "Sepal.Length"))
+print(sum_column(iris, "Species"))
+print(sum_column(warpbreaks, "breaks"))
+
+## 3.b ----
+
+# Sum values in a vector.
+#
+# ARGUMENTS:
+# x: a vector
+#
+# RETURN VALUE:
+# if the vector contains numbers, returns the sum of
+# all values; otherwise, returns NULL
+
+my_sum <- function(x) {
+  # On assigne à "my_sum" la fonction ayant x pour argument
+  if (is.numeric(x)){
+    # On établit une condition ayant comme contrainte "x est numérique"
+    result <- 0 
+    # et un résultat par défaut (0)
+    for (i in x){
+      # Ouverture d'une boucle qui s'applique successivement à toutes les valeurs contenues dans x
+      result <- result + i }
+    # On assigne un nouveau résultat tel que pour la première occurence de i dans x,
+    # ce résultat soit égal à O + i. La boucle permet une addition incrémentale, à chaque
+    # nouvelle valeur contenue dans x, celle ci s'ajoute à la somme précédente et ainsi de suite.
+    return(result)
+    # On obtient le résultat final lorsque la boucle a traité l'ensemble des i de x.
+  }
+  else {return(NULL)}
+  # Si les valeurs ne sont pas numériques, on obtient "NULL"
+}
+
+print(my_sum(iris$Sepal.Length))
+print(my_sum(iris$Species))
+print(my_sum(warpbreaks$breaks))
+
+## 3.c ----
+
+# Quotient d'une somme par un nombre
+#
+# ARGUMENTS:
+# x: vecteur
+# k: nombre 
+#
+# RETURN VALUE:
+# Si x et k sont numériques, retourne le quotient de la somme des valeurs 
+# contenues dans le vecteur x et de k. Sinon, NULL. 
+
+sum_divided_by <- function(x, k){
+# On assigne à "sum_divided_by" une fonction à deux arguments
+    if (is.numeric(x) & is.numeric(k)){
+# On établit une double condition : x ∧ k = TRUE
+    result <- (my_sum(x)/k)
+# Pour cette condition, on paramètre le résultat en reprenant celui de la fonction my_sum,
+# et en le divisant par k
+    }   
+    else {return(NULL)}
+# Si x et k ne sont pas tous les deux numériques, alors obtient NULL
+}
+
+print(sum_divided_by(iris$Sepal.Length, 12))
+print(sum_divided_by(iris$Species, 22))
+print(sum_divided_by(iris$Sepal.Length, "Not numeric"))
+print(sum_divided_by(warpbreaks$breaks, -12))
+
+## 3.d ----
+
+# Moyenne d'un vecteur
+#
+# ARGUMENTS:
+# x: vecteur
+# k: nombre 
+#
+# RETURN VALUE:
+# Si x est numérique, retourne le quotient de la somme de x par sa longueur 
+# Sinon, NULL. 
+
+my_mean <- function(x){
+# On assigne à "my_mean" une fonction à un argument
+  if (is.numeric(x)) {
+# On établit une condition tel que : x est numérique
+    y <- length(x)
+# On assigne à la variable y le nombre correspondant à la longueur du vecteur x
+    result <- sum_divided_by(x, y) 
+# On paramètre notre résultat en réutilisant la foncntion "sum_divides_by",
+# pour obtenir le quotient de la somme du vecteur par sa longueur
+    }   
+  else {return(NULL)}
+# Si x n'est pas numérique, NULL
+  }
+      
+cols  
+print(my_mean(iris$Sepal.Length))
+print(my_mean(iris$Species))
+print(my_mean(warpbreaks$breaks))
+
